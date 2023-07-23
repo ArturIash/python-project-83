b48c85f361b8e2c2e9d45b28200aeb94ef2fbcf3
ArturIash <4ezyyy@gmail.com>
2023-07-23 16:47:54 +0300

    update code

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+import os
+import logging
+import requests
+import psycopg2
+
+from datetime import datetime
+from psycopg2.extras import NamedTupleCursor
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    request,
+    flash,
+    url_for,
+)
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET
 
 @app.route("/")
-def hello_world():
-    return "<p>ЖЕКИЧ GO SLEEP</p>"
+def index():
+    return render_template("index.html")
