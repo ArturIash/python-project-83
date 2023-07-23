4fb381a4c4fe15a9d702aa146423d9d2294e86d8
ArturIash <4ezyyy@gmail.com>
2023-07-23 16:41:49 +0300

    add parser.py

---
+++ page_analyzer/parser.py
@@ -0,0 +1,7 @@
+def get_data_from_html(html_content) -> tuple:
+    h1 = html_content.h1.get_text() if html_content.h1 else ""
+    title = html_content.title.get_text() if html_content.title else ""
+    description = html_content.find("meta", {"name": "description"})
+    description_content = description["content"] if description else ""
+
+    return h1, title, description_content
